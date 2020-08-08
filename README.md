5fab04e81849fa03cd6090aca9667d4ed58f32fe
aymaru <aymaru93@gmail.com>
2020-08-07 20:43:43 -0600

    actualizado!

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 acertijo logica simple - prolog
+
+Objetivo del programa: Resolver el siguiente acertijo.
+
+    Tres amigas se reúnen a tomar el té. Sus apellidos, ocupaciones y nombres son
+todos diferentes. Se sabe que Beatriz no es García, la señora de apellido López es
+secretaria en una oficina, la actriz se llama Claudia y la maestra no es Méndez.
+Alicia llegó antes que las demás amigas 
+
+¿Cúal es el nombre y apellido de cada amiga y a qué se dedican cada una?
+
+Para ejecutar usando swipl en linux usar el comando:
+    swipl acertijo.pl
--- acertijo.pl
+++ acertijo.pl
@@ -1,7 +1,6 @@
 /*---- BASE DE CONOCIMIENTOS ----*/
 
 /*- PROPIEDADES -*/
-/* prof(Profesor) <- Profesor es el nombre de un profesor */
 nombre(beatriz).
 nombre(claudia).
 nombre(alicia).
@@ -62,11 +61,8 @@ relacion_apellido_profesion(Apellido,Profesion):- apellido(Apellido),  profesion
 /*---- PROGRAMA PRINCIPAL ----*/
 /* iniciar <- llamada inicial del programa */
 iniciar:- !,ini(3,[],[],[]).
-/* ini(M,Lista1,Lista2,Lista3) 
-    M es la cantidad de elementos
-    Las listas contienen la solucion al problema
-*/
-/* Correlacionar 3 propiedades */
+
+/* Correlacionar las propiedades de nombre, apellido y profesion propiedades */
 ini(M,ListaNombre,ListaApellido,ListaProfesion) :- nel(ListaNombre,M),escribir(ListaNombre,ListaApellido,ListaProfesion),nl.
 ini(M,ListaNombre,ListaApellido,ListaProfesion) :- relacion_nombre_profesion(Nombre,Profesion),
  nopertenece(Nombre,ListaNombre),
@@ -77,30 +73,3 @@ relacion_apellido_profesion(Apellido,Profesion),
 ini(M,[Nombre|ListaNombre],[Apellido|ListaApellido],[Profesion|ListaProfesion]).
 
 :- mensaje.
-% prof(maria).
-% prof(jesus).
-% prof(faraon).
-
-% /* asig(Asignatura) <- Asignatura es el nombre de una asignatura */
-% asig(logica).
-% asig(programacion).
-% asig(matematicas).
-
-% /* dia(Dia) <- Dia es un día de la semana que hay alguna clase */
-% dia(lunes).
-% dia(miercoles).
-% dia(jueves).
-% /*- RELACIONES -*/
-% /* r1(Profesor,Asignatura) <- Profesor imparte la Asignatura */
-% r1(maria,programacion).
-% r1(Profesor,Asignatura) :- prof(Profesor), Profesor\=maria,
-%  asig(Asignatura).
-% /* r2(Profesor,Dia) <- Profesor imparte sus clases el Dia de la semana */
-% r2(faraon,Dia) :- dia(Dia), Dia\=lunes.
-% r2(Profesor,Dia) :- prof(Profesor), Profesor\=faraon,
-%  dia(Dia).
-% /* r3(Asignatura,Dia) <- Asignatura se imparte el Dia de la semana */
-% r3(logica,Dia) :- dia(Dia), Dia\=lunes, Dia\=jueves.
-% r3(programacion,Dia) :- dia(Dia), Dia\=lunes.
-% r3(Asignatura,Dia) :- asig(Asignatura), Asignatura\=logica,
-%  Asignatura\=programacion, dia(Dia).
